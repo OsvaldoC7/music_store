738499e647eea88e472d9c94e84042e360999cf2
OsvaldoC7 <osva.6cruz@gmail.com>
2021-11-13 17:32:01 -0600

    Seeders and factories

---
+++ database/factories/ArticuloFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Articulo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticuloFactory extends Factory {
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Articulo::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre'=>$this->faker->words(2, true),
+            'artista'=>$this->faker->name(),
+            'lanzamiento'=>$this->faker->date(),
+            'descripcion'=>$this->faker->paragraph(),
+            'cantidad'=>$this->faker->randomNumber(3, false),
+            'precio'=>$this->faker->randomFloat(2, 10, 1500),
+            'codigo'=>'codigo_preuba',
+            'foto'=>'1',
+            'mime'=>'mime_prueba'
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Articulo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +14,23 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        
+        User::create([
+            'name'=>'Admin',
+            'email'=>'admin.admin@admin.admin',
+            'password'=>Hash::make('123456789'),
+            'utype'=>'ADM'
+        ]);
+
+        User::create([
+            'name'=>'Osvaldo',
+            'email'=>'osva.6cruz@gmail.com',
+            'password'=>Hash::make('123456789')
+        ]);
+
+        User::factory(10)->create();
+        Articulo::factory(10)->create();
+
     }
 }
