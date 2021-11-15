d6f877b562a902dbcad9cfc3778464cbdb7ef680
OsvaldoC7 <osva.6cruz@gmail.com>
2021-11-15 09:55:35 -0600

    Adding generos through a seeder

--- app/Models/Genero.php
+++ app/Models/Genero.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Genero extends Model {
 
     use HasFactory;
+    public $timestamps = false;
 
     public function articulos() {
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Articulo;
+use App\Models\Genero;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -31,6 +32,38 @@ class DatabaseSeeder extends Seeder
 
         User::factory(10)->create();
         Articulo::factory(10)->create();
+
+        /* Creando los generos */
+        Genero::create([
+            'nombre_genero'=>'Pop'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Country'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Rock'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Electro'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Folk'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Indie'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Clasica'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Jazz'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'R&B'
+        ]);
+        Genero::create([
+            'nombre_genero'=>'Metal'
+        ]);
 
     }
 }
